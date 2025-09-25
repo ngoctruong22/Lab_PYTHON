cdbf64cda590be93ac8bc8c6cbcd0846529d674f
ngoctruong22 <ngoctruong220305@gmail.com>
2025-09-25 22:24:39 +0300

    Labpython

---
+++ main.py
@@ -0,0 +1,10 @@
+def twoSum(nums, target):
+    if not nums:
+        return None
+    if not all(isinstance(num, int) for num in nums):
+        return None
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+    return None
