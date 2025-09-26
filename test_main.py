ad99e2e053f8175ae9d8f4b84e1eda2c14aa2d0a
ngoctruong22 <ngoctruongdinga@gmail.com>
2025-09-26 17:33:17 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -3,17 +3,14 @@ from main import twoSum
 
 
 class TestTwoSum(unittest.TestCase):
-
+    # Стандартный тест из примера 
     def test_example1(self):
-        # Стандартный тест из примера LeetCode
         self.assertEqual(twoSum([2, 7, 11, 15], 9), [0, 1])
 
     def test_example2(self):
-        # Тест с числами в разном порядке
         self.assertEqual(twoSum([3, 2, 4], 6), [1, 2])
 
     def test_example3(self):
-        # Тест с одинаковыми числами
         self.assertEqual(twoSum([3, 3], 6), [0, 1])
 
     def test_negative(self):
