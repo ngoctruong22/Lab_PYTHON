f1d3b2c17585b175bb2f1663ec751ce4fe2fc66e
ngoctruong22 <ngoctruong220305@gmail.com>
2025-09-26 17:26:32 +0300

    fix conflict

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 # Функция, которую будем тестировать
 def twoSum(nums, target):
-<<<<<<< HEAD
     # Проверка, что все элементы являются целыми числами
-=======
-    # push github
-    if not nums:
-        return None
->>>>>>> 3f4694486348720836c5d2585a331ca606c3b1ef
     if not all(isinstance(num, int) for num in nums):
         return None
 
